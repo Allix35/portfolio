98af55b186d7a9ac0457db22391a98a7b674e071
Allix Dolou <dolou.allix@gmail.com>
2025-05-22 16:04:22 +0200

    ajout de purge css

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import purgecss from 'vite-plugin-purgecss';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), purgecss()],
   base: '/portfolio/', 
 })
 
